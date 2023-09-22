e116daa0df6b8366ef363d4807be90a7d2c7a4a8
Abhishek Handore <abhihandore2703@gmail.com>
2023-09-22 14:27:17 +0530

    Dark Theme Issue Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body id="root" className={`${figtree.className} ${figtree.variable}`}>
+      <body
+        id="root"
+        className={`${figtree.className} ${figtree.variable} text-black`}
+      >
         <main>
           <Header />
           {children}
